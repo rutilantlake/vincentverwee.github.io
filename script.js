f29bf4d296f118ee3a62bac6d943735a5dc1bb51
rutilantlake <68325553+rutilantlake@users.noreply.github.com>
2023-10-08 15:03:05 +0200

    test commit in vc

---
+++ script.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function () {
+    var button = document.getElementById("clickMe");
+
+    // listener
+    button.addEventListener("click", function () {
+        // execute when clicked
+        alert("milan eeft dikke akka man");
+    });
+});
